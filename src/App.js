347d292d90455b938dc016cc76b535fdf96746e8
Vaibhav Dixit <vaibhavdixit107@gmail.com>
2018-04-05 21:20:39 -0600

    Add react-bootstrap package, add moving functionality and the reset function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import * as B from 'react-bootstrap';
 
 //Making the Squares in the grid
 function Square(props) {
@@ -41,6 +41,51 @@ class Board extends Component {
     this.state = JSON.parse(JSON.stringify(initialState));// Make a copy of the initial state
   }
   
+  //To reset the state of the game to initial state
+  reset(){
+    this.setState(initialState);  
+  }
+  //To handle the click to make the move in the grid
+  handleClick(i){
+    this.makeMove(i)
+  }
+
+  //To make the move in the board
+  makeMove(columnId){
+    let {columnHeight} = this.state;
+    let {history} = this.state;
+    let {stepNumber} = this.state;
+    let {isNextRed} = this.state;
+
+    
+
+    const boardCopy = history[stepNumber].boardState.map(function(arr) {
+      return arr.slice();
+    });
+
+    //To start filling the box from bottom
+    let newColumnHeight = boardCopy[columnId].reverse();
+    
+    //Slice the history to the step clicked
+    if(history.length > stepNumber+1 ){
+      history = history.slice(0,stepNumber+1);
+    } 
+
+    if(columnHeight[columnId] < 6){  
+      boardCopy[columnId][columnHeight[columnId]] = isNextRed ? 'Red':'Blue';
+      newColumnHeight.reverse();
+      history.push({boardState:boardCopy});
+      columnHeight[columnId]++;
+      stepNumber++;
+      isNextRed = isNextRed?false:true;
+
+      this.setState({history : history});
+      this.setState({stepNumber : stepNumber});
+      this.setState({isNextRed : isNextRed });
+      this.setState({columnHeight : columnHeight});
+    }
+  }
+
   render() {
     const {stepNumber} = this.state;
     let board = this.state.history[stepNumber].boardState;
@@ -54,17 +99,15 @@ class Board extends Component {
  
     return (
     <div>
- 
+      <B.Button bsStyle="primary" bsSize="large" onClick={()=>this.reset()} className="btnMove">Reset</B.Button>{/*Button to reset the game to initial state*/}
       <div className="Board">
         {columns}{/* Drawing the columns*/}
-      </div>
- 
+      </div> 
     </div>  
     );
-
-
   }
 }
+
 class App extends Component {
   render() {
     return (

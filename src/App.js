04c40ede1a6424bc058cc77ccdd57b7fbe53f25a
Vaibhav Dixit <vaibhavdixit107@gmail.com>
2018-04-05 21:51:54 -0600

    trackHistory

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import * as B from 'react-bootstrap';
 
+
+
 //Making the Squares in the grid
 function Square(props) {
   return (
@@ -33,31 +35,35 @@ const initialState = {
       winner: ''
 }
 
-
 //making the board for the game
 class Board extends Component {
   constructor() {
     super();
     this.state = JSON.parse(JSON.stringify(initialState));// Make a copy of the initial state
   }
-  
-  //To reset the state of the game to initial state
-  reset(){
-    this.setState(initialState);  
-  }
+
   //To handle the click to make the move in the grid
   handleClick(i){
     this.makeMove(i)
+  }
+
+  //To reset the state of the game to initial state
+  reset(){
+    this.setState(initialState);  
   }
 
   //To make the move in the board
   makeMove(columnId){
     let {columnHeight} = this.state;
     let {history} = this.state;
+    let {winner} = this.state;
     let {stepNumber} = this.state;
     let {isNextRed} = this.state;
 
-    
+    //if there is a winner 
+    if(winner !== ""){
+      return;
+    }
 
     const boardCopy = history[stepNumber].boardState.map(function(arr) {
       return arr.slice();
@@ -83,11 +89,40 @@ class Board extends Component {
       this.setState({stepNumber : stepNumber});
       this.setState({isNextRed : isNextRed });
       this.setState({columnHeight : columnHeight});
+      this.setState({winner : winner});
     }
   }
 
+  //To track the history of the game
+  trackHistory(boardState,index){
+    if(index!=null){
+      let {history} = this.state;
+      let board = this.state.history[index].boardState;
+      let columnHeightArray = [];// array for the current height of the column
+      for(let i=0;i<board.length;i++){
+        let currentColumnHeight = board[i].findIndex(f => f === 0);
+        if(currentColumnHeight <= 0){
+          columnHeightArray.push(0);
+        }else{
+          columnHeightArray.push(currentColumnHeight);
+        }  
+      }
+      
+      return(
+        this.setState({
+          stepNumber : index,
+          history : history,
+          isNextRed : (index % 2) === 0,
+          columnHeight : columnHeightArray
+        })
+      )
+    }
+  }
+
+
   render() {
     const {stepNumber} = this.state;
+    const {history} = this.state;
     let board = this.state.history[stepNumber].boardState;
     let columns = [...Array(board.length)].map((x, i) => 
       <ColumnHeight 
@@ -96,6 +131,18 @@ class Board extends Component {
         handleClick={() => this.handleClick(i)}>
       </ColumnHeight>
     )
+
+     {/*Tracking the move index*/}
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move} className="History">
+          <button onClick={() => this.trackHistory(step.boardState,move)} className="btnMove">{desc}</button>
+        </li>
+      );
+    });
  
     return (
     <div>
@@ -103,6 +150,7 @@ class Board extends Component {
       <div className="Board">
         {columns}{/* Drawing the columns*/}
       </div> 
+      <p>{moves}</p>{/*Displaying the move index in the button*/}
     </div>  
     );
   }
